6a91813ff473af9eb1222478c889088d13c48868
Pablorl <Pablorodlom@gmail.com>
2023-04-17 16:44:29 +0200

    feat: trabajando en xml,side bar echa trabajando en el fxml de los bug fossil y fish

---
+++ src/main/java/com/example/proyectoud1pablorl/FossilController.java
@@ -0,0 +1,4 @@
+package com.example.proyectoud1pablorl;
+
+public class FossilController {
+}
--- src/main/java/com/example/proyectoud1pablorl/HelloController.java
+++ src/main/java/com/example/proyectoud1pablorl/HelloController.java
@@ -1,23 +1,91 @@
 package com.example.proyectoud1pablorl;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController  implements Initializable {
 
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+
+
+    @FXML
+    private AnchorPane pane1,paneOptions;
+
+
+    @FXML
+    private ImageView exit,menu;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        exit.setOnMouseClicked(mouseEvent -> {
+            System.exit(0);
+        });
+
+        pane1.setVisible(false);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5),pane1);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5),paneOptions);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+
+        menu.setOnMouseClicked(mouseEvent -> {
+
+            pane1.setVisible(true);
+
+            FadeTransition fadeTransition1= new FadeTransition(Duration.seconds(0.5),pane1);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(0.15);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1= new TranslateTransition(Duration.seconds(0.5),paneOptions);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();
+        });
+
+        pane1.setOnMouseClicked(mouseEvent -> {
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
+            fadeTransition1.setFromValue(0.15);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(actionEvent -> {
+                pane1.setVisible(false);
+            });
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),paneOptions);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();
+
+
+        });
+    }
 
 
     public void switchToFish(MouseEvent event) throws IOException {
@@ -28,17 +96,19 @@ public class HelloController {
         stage.show();
     }
 
+    public void switchToFossil(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Fossil.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-
-
-
-
-
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchToBug(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Bug.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
