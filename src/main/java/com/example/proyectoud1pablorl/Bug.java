9093dabb516912296233dda23b9a19d60e170b58
Pablorl <Pablorodlom@gmail.com>
2023-04-20 00:34:33 +0200

    feat:pojos de todas las clases

--- src/main/java/com/example/proyectoud1pablorl/Bug.java
+++ src/main/java/com/example/proyectoud1pablorl/Bug.java
@@ -1,58 +1,11 @@
 package com.example.proyectoud1pablorl;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
 public class Bug{
-	public int priceFlick;
-	public String catchPhrase;
-	public String imageUri;
-	public int price;
-	public Name name;
-	public String museumPhrase;
-	public int id;
+	private List<BugItem> bug;
 
-	@JsonIgnoreProperties
-	public String fileName;
-	public Availability availability;
-	public String iconUri;
-
-	public int getPriceFlick(){
-		return priceFlick;
-	}
-
-	public String getCatchPhrase(){
-		return catchPhrase;
-	}
-
-	public String getImageUri(){
-		return imageUri;
-	}
-
-	public int getPrice(){
-		return price;
-	}
-
-	public Name getName(){
-		return name;
-	}
-
-	public String getMuseumPhrase(){
-		return museumPhrase;
-	}
-
-	public int getId(){
-		return id;
-	}
-
-	public String getFileName(){
-		return fileName;
-	}
-
-	public Availability getAvailability(){
-		return availability;
-	}
-
-	public String getIconUri(){
-		return iconUri;
+	public List<BugItem> getBug(){
+		return bug;
 	}
 }
